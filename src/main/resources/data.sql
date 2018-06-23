eb595ce6c28259c631ccdddfd23c7ddc1b440366
hupadhyay <himanshu2703@gmail.com>
2018-06-23 01:41:21 -0400

    updated

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into PERSON (ID, NAME, CITY, GENDER, AGE) values (101,'Himanshu', 'Lucknow', 'Male', 36)
+insert into PERSON (ID, NAME, CITY, GENDER, AGE) values (102, 'Anushka', 'Lucknow', 'Female', 26)
+insert into PERSON (ID, NAME, CITY, GENDER, AGE) values (103, 'Hritika', 'Lucknow', 'Female', 5)
+insert into PERSON (ID, NAME, CITY, GENDER, AGE) values (104, 'Hrishik', 'Lucknow', 'Male', 3)
